68e620505e44f0b78f03795ea120187013b22eeb
MetaCorp <metane.corp@gmail.com>
2014-01-18 19:22:59 +0100

    fix

---
+++ src/main/c/network.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "../headers/network.h"
+
+void print_layer(vector *layer, size_t n)
+{
+    printf("    layer %2lu : ", n);
+
+    for(size_t i = 0; i<layer->size; ++i)
+        printf("%5f ", at(layer, i));
+    
+    printf("/n");
+}
+
+void pretty_print(network *n)
+{
+    printf("Print network - %lu layers\n", n->depth);
+
+    for(size_t i = 0; i<n->depth; ++i)
+        print_layer(n->layers[i], i);
+
+    printf("End print network\n");
+}
