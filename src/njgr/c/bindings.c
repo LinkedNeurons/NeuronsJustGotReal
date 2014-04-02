566d60712b0f00bab51bb548c1f1ecdd201472b2
KingGreed <gauber_r@epita.fr>
2014-04-02 21:20:09 +0200

    Changed names

--- src/njgr/c/bindings.c
+++ src/njgr/c/bindings.c
@@ -5,7 +5,7 @@
 #include "math/vector.h"
 #include "network.h"
 
-value camel_feed(value network, value dist, value height) {
+value bindings_feed(value network, value dist, value height) {
 	CAMLparam3(network, dist, height);
 	Vector* v = IVector.create(2);
 	v->tab[0] = (double) Int_val(dist); v->tab[1] = (double) Int_val(height);
@@ -16,7 +16,7 @@ value camel_feed(value network, value dist, value height) {
 	CAMLreturn(Val_bool(IVector.at(out, 0) >= 0));
 }
 
-value camel_getNeuralNetwork(value unit) {
+value bindings_getNeuralNetwork(value unit) {
 	CAMLparam1(unit);
 	Network* network = NULL; //TODO : create neural network here
 	CAMLreturn(Val_long(network));
