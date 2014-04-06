a95dd4b08a412147674874d62c6fbf0d1abb0a44
KingGreed <gauber_r@epita.fr>
2014-04-06 19:47:42 +0200

    Implemented move line and move column

--- src/sample_2048/c/main.c
+++ src/sample_2048/c/main.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
+#include <time.h>
 
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
@@ -9,17 +9,100 @@
 #define HEIGHT 700
 #define COLORS 32
 
+#define GRID_SIZE 16
+
+#define LEFT  0
+#define RIGHT 1
+#define UP    2
+#define DOWN  3
+
 typedef struct {
 	SDL_Surface *display;
 	SDL_Surface *grid, *tile;
 } Assets;
 
+typedef struct {
+	int grid[4][4];
+	int lost; 
+} Grid;
 
-Assets *ass = NULL; 
+typedef struct {
+	Grid *grids;
+	int   size;
+} Gameboard;
+
+Assets *ass     = NULL;
+Gameboard *game = NULL;
+
+void init_gameboard(int grids) {
+	game = malloc(sizeof(Gameboard));
+	game->grids = malloc(grids * sizeof(Grid));
+
+	for (int i = 0; i < grids; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			for (int k = 0; k < 4; ++k)
+			{
+				game->grids[i].grid[j][k] = 0;
+			}
+		}
+		game->grids[i].lost = 0;
+
+		int first = rand() % GRID_SIZE, second = 0;
+		do {
+			second = rand() % GRID_SIZE;
+		} while (second == first);
+		game->grids[i].grid[first  / 4][first  % 4] = 1;
+		game->grids[i].grid[second / 4][second % 4] = 1;
+	}
+}
+
+void moveColum(Grid *grid, int column, int up) {
+
+	int start, direction, stop;
+	if(up == 1) { start = 0; direction = 1;  stop = 3; }
+	else 		{ start = 3; direction = -1; stop = 0; }
+
+	for (int i = start; i != stop; i = i + direction) {
+		if (i != stop && grid->grid[column][i + direction] == grid->grid[column][i]) {
+			++grid->grid[column][i];
+			grid->grid[column][i + direction] = 0;
+		}
+	}
+	for (int i = start; i != stop; i = i + direction) {
+		if(i != stop && grid->grid[column][i] == 0) {
+			grid->grid[column][i] = grid->grid[column][i + direction];
+			grid->grid[column][i + direction] = 0;
+		}
+	}
+}
+
+void moveLine(Grid *grid, int line, int left) {
+	int start, direction, stop;
+	if(left == 1) { start = 0; direction = 1;  stop = 3; }
+	else 		{ start = 3; direction = -1; stop = 0; }
+
+	for (int i = start; i != stop; i = i + direction) {
+		if (i != stop && grid->grid[i + direction][line] == grid->grid[i][line]) {
+			++grid->grid[i][line];
+			grid->grid[i + direction][line] = 0;
+		}
+	}
+	for (int i = start; i != stop; i = i + direction) {
+		if(i != stop && grid->grid[i][line] == 0) {
+			grid->grid[i][line] = grid->grid[i + direction][line];
+			grid->grid[i + direction][line] = 0;
+		}
+	}
+}
+
+void moveGrid(int direction) {
+
+}
 
 void init() {
 
 	ass = malloc(sizeof(Assets));
+	init_gameboard(9);
 
 	SDL_Init(SDL_INIT_VIDEO);
 	ass->display = SDL_SetVideoMode(WIDTH, HEIGHT, COLORS, SDL_HWSURFACE);
@@ -35,7 +118,9 @@ void init() {
 
 
 void draw() {
-
+	for (int i = 0; i < game->size; ++i) {
+		
+	}
 	SDL_Flip(ass->display);
 }
 
@@ -47,7 +132,12 @@ void destroy() {
 	SDL_FreeSurface(ass->display);
 	SDL_FreeSurface(ass->grid);
 	SDL_FreeSurface(ass->tile);
+	
 	free(ass);
+
+	free(game->grids);
+	free(game);
+
 	SDL_Quit();
 }
 
@@ -71,6 +161,7 @@ void catchKillEvent(int *killed) {
 
 int main(int argc, char *argv) {
 	printf("\n\n \t2048 Sample logs \n\n");
+	srand(42);
 	init();
 
 	int killed = 0;
