34d648b401b1320f6b667320d20800465c7b1456
Snaipe <franklinmathieu@gmail.com>
2014-04-08 17:02:42 +0200

    Beware of copypaste

--- src/test/headers/genetics/test_population.h
+++ src/test/headers/genetics/test_population.h
@@ -1,7 +1,7 @@
 void population_test_generation();
 
 int add_population_tests() {
-    CU_pSuite suite = CU_add_suite("Matrix", NULL, NULL);
+    CU_pSuite suite = CU_add_suite("Population", NULL, NULL);
 
     return suite
         && CU_add_test(suite, "population_next_generation", &population_test_generation);
